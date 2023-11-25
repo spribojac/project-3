857604d1d317210fdb450f0d2fb7c642975c0f35
spribojac <sophie170995@live.co.uk>
2023-11-25 15:50:22 +0000

    new app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine, text, inspect, func, MetaData, Table, Colu
 from flask import Flask, render_template
 import json
  
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, jsonify
 
 #################################################
 # Database Setup
@@ -54,14 +54,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/extract<br/>"
-        f"/songs"
-    )
+   
+   return render_template("index.html")
 
-@app.route("/extract")
+@app.route("/pie")
+def pie():
+    return render_template("pie.html")
+
+@app.route("/songs_extract") # remove as route maybe?
 def extract():
     # Create our session (link) from Python to the DB
     session = Session(engine)
@@ -104,7 +104,7 @@ def extract():
     with open('static/spotify_songs.json', 'w') as file2:
         json.dump(all_songs, file2)
 
-    return render_template('index.html')
+    return render_template('songs_extract.html')
 
 @app.route("/songs")
 def songs():
@@ -119,6 +119,52 @@ def songs():
     for song in results]
 
     return jsonify(all_songs)
+
+# Function to get paginated results
+def get_paginated_results(query, page=1, per_page=25):
+    offset = (page - 1) * per_page
+    return query.offset(offset).limit(per_page).all()
+
+@app.route('/search', methods=['GET'])
+def search_form():
+    return render_template('search_form.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    session = Session(engine)
+
+    search_query = request.form.get('search_query', '')
+    page = int(request.form.get('page', 1))
+
+    # Assuming SpotifySongs is your SQLAlchemy model
+    query = session.query(SpotifySongs).filter(
+        (SpotifySongs.track_name.ilike(f'%{search_query}%')) |
+        (SpotifySongs.artist.ilike(f'%{search_query}%'))
+    )
+
+    total_results = query.count()
+    paginated_results = get_paginated_results(query, page)
+
+    session.close()
+
+    if paginated_results:
+        # Return a list of matching songs as JSON
+        result_list = [{
+            "track_name": song.track_name,
+            "artist": song.artist,
+            "popularity": song.popularity,
+            # Include additional details as needed
+        } for song in paginated_results]
+
+        return jsonify({
+            "total_results": total_results,
+            "per_page": 25,
+            "current_page": page,
+            "results": result_list,
+        })
+
+    # Return a JSON indicating that no matching songs were found
+    return jsonify({"error": "No matching songs found"})
 
 if __name__ == '__main__':
     app.run(debug=True)
